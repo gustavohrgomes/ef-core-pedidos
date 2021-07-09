15999e63878e1d3e3eede36c0de029330e81ce57
Gustavo Gomes <dev.gustavogomes@gmail.com>
2021-07-09 15:48:54 -0300

    Add email migration

---
+++ Pedidos/Migrations/20210709182453_AddedEmail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Pedidos.Migrations
+{
+    public partial class AddedEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Clientes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Clientes");
+        }
+    }
+}
